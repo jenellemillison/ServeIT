b9417780a00e36e2299fb2b96cf8fe7ce074f918
jenellemillison <JAMillison@comcast.net>
2021-10-17 01:09:49 -0500

    added the totals to the business data so that we can get total payroll to be target variable

--- data_cleaning/compile_business_csvs.py
+++ data_cleaning/compile_business_csvs.py
@@ -10,6 +10,7 @@ import pandas as pd
 COL_BUSINESS_DATA = ['State', 'State_Name', 'County', 'County_Name', 'NAICS', 'NAICS_Description', 'Enterprise_Size', 'Firms', 'Establishments', 'Employment', 'Employment_Noise_Flag', 'Annual_Payroll', 'Annual_Payroll_Noise_Flag']
 SERVICE_INDUSTRIES = ['Food Services and Drinking Places', 'Accommodation and Food Services', 'Accommodation', 'Food and Beverage Stores', 'Retail Trade', 'General Merchandise Stores', 'Clothing and Clothing Accessories Stores']
 ENTERPRISE = ['2: <20 employees', '3: 20-99 employees']
+TOTAL = ['1: Total']
 
 serveIT_df = pd.DataFrame()
 
@@ -35,12 +36,20 @@ business_data_clean['Firms'].astype(int)
 #dont need the state_name anymore because all are TX
 business_data_clean.drop('State_Name', axis=1, inplace=True)
 
+#extract out county totals
+business_data_clean_total = business_data_clean[business_data_clean['NAICS_Description'] == 'Total']
+business_data_clean_total = business_data_clean_total[business_data_clean_total['Enterprise_Size'].isin(TOTAL)]
+business_data_clean_total = business_data_clean_total.drop('Enterprise_Size', axis=1)
+
 #filter out everything that is not service industry
 business_data_clean = business_data_clean[business_data_clean['NAICS_Description'].isin(SERVICE_INDUSTRIES)]
-
 #filter out big business and combine stats for everything under 100 employees
 business_data_clean = business_data_clean[business_data_clean['Enterprise_Size'].isin(ENTERPRISE)]
 aggregation_functions = {'Firms':'sum', 'Establishments':'sum', 'Employment':'sum', 'Annual_Payroll':'sum'}
-business_data_clean = business_data_clean.groupby(['County_Name', 'NAICS_Description'], as_index=False).aggregate(aggregation_functions)
+business_data_clean_agg = business_data_clean.groupby(['County_Name', 'NAICS_Description'], as_index=False).aggregate(aggregation_functions)
 
-business_data_clean.to_csv('../cleaned_data/2019_tx_county_business_data.csv', index=False)
+#combine and sort small businesses and total
+business_data_clean_final = business_data_clean_agg.append(business_data_clean_total)
+business_data_clean_final.sort_values(by=["County_Name"], inplace = True)
+
+business_data_clean_final.to_csv('../cleaned_data/2019_tx_county_final_business_data.csv', index=False)
